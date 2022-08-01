d3eb8e484d530f8eadd06f5c5c40c5b24ad316d2
Reptar007 <sebe4106@gmail.com>
2022-08-01 16:46:30 -0400

    Partial Phase 2

--- practices/01-timing-benchmarks/phase-2.js
+++ practices/01-timing-benchmarks/phase-2.js
@@ -2,8 +2,23 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  let myArr = []
+  let i = increment
+  let counter = 0
+  while (counter < 10) {
+    myArr.push(addNums(i));
+    i += i
+    counter++
+  }
+  // for (let i = 1; i <= 10; i += increment){
+  //   myArr.push(addNums(i))
+  // }
+  return myArr
 }
+
+sums = addNums10(10)
+
+console.log(sums)
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
